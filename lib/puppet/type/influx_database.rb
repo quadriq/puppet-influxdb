797d852fdf6b1b3acc3fc9686cc960604f2fd848
Ott, Jörn WI (Fa. Allgeier Experts / Goetzfried) <Joern.Ott@schufa.de>
2020-05-13 16:36:39 +0200

    more rubocop

--- lib/puppet/type/influx_database.rb
+++ lib/puppet/type/influx_database.rb
@@ -1,19 +1,18 @@
 Puppet::Type.newtype(:influx_database) do
-  @doc = "Manages Databases"
+  @doc = 'Manages Databases'
   ensurable
 
   newparam(:name, :namevar => true) do
-    desc "name of user account"
+    desc 'name of user account'
   end
 
   newparam(:superuser) do
-    desc "Auth superuser"
+    desc 'Auth superuser'
     defaultto(:undef)
   end
 
   newparam(:superpass) do
-    desc "Auth superpass"
+    desc 'Auth superpass'
     defaultto(:undef)
   end
-  
 end
