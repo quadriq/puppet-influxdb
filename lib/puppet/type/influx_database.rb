0731d2f8980ce10e279ef4bec17d203ef2237dfd
Ott, Jörn WI (Fa. Allgeier Experts / Goetzfried) <Joern.Ott@schufa.de>
2020-05-13 17:01:02 +0200

    last rubocop issue

--- lib/puppet/type/influx_database.rb
+++ lib/puppet/type/influx_database.rb
@@ -2,7 +2,7 @@ Puppet::Type.newtype(:influx_database) do
   @doc = 'Manages Databases'
   ensurable
 
-  newparam(:name, :namevar => true) do
+  newparam(:name, namevar: true) do
     desc 'name of user account'
   end
 
